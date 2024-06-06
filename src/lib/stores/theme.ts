457381a0eb9f27314ece0157cd13a5ca8f37207f
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-06-06 21:40:14 +0100

    chore: add state for management audio playing & theming

---
+++ src/lib/stores/theme.ts
@@ -0,0 +1,21 @@
+import { writable } from "svelte/store";
+import { browser } from "$app/environment";
+
+type Theme = "light" | "dark";
+
+let initialTheme: Theme = "light";
+
+if (browser) {
+	const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+	const defaultValue: Theme = prefersDarkMode.matches ? "dark" : "light";
+	initialTheme = (window.localStorage.getItem("theme") as Theme) ?? defaultValue;
+}
+
+export const theme = writable<Theme>(initialTheme);
+
+theme.subscribe((value) => {
+	if (browser) {
+		localStorage.setItem("theme", value);
+		document.body.classList.toggle("dark", value === "dark");
+	}
+});
