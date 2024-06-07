0632f5c6907d589b373cad64ce57f222b2c1e5fb
Ibrahim Raimi <raimiibrahim44@gmail.com>
2024-06-07 10:10:52 +0100

    feat: add API routes for getting audio features and cover art

---
+++ src/routes/api/get-cover/+server.ts
@@ -0,0 +1,37 @@
+import { supabase } from "$lib/supabase";
+
+export type ExistingCover = {
+	original: {
+		name: string;
+		artists: string[];
+	};
+	cover: {
+		artists: string[];
+	};
+	slug: string;
+	created_at: string;
+};
+
+export async function GET({ url }) {
+	const id = url.searchParams.get("id");
+
+	if (!id) {
+		throw new Error("No ID provided");
+	}
+
+	const { data: existingCover } = await supabase
+		.from("covers")
+		.select(
+			`slug,
+      created_at,
+      original:original_id(name, artists),
+      cover:cover_id(artists)`
+		)
+		.eq("cover_id", id)
+		.returns<ExistingCover>()
+		.single();
+
+	if (!existingCover) return Response.json(null);
+
+	return Response.json(existingCover);
+}
